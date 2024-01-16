dfa0004cdb0885c44258918aa5e743e19f896ab7
mostsfa538 <mostafa51moktar@gmail.com>
2024-01-16 01:10:25 -0800

    nop

---
+++ _nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything
+ * @stack: the stack
+ * @data: data.
+ *
+ * Return: void
+ */
+void _nop(stack_t **stack, unsigned int data)
+{
+	(void)stack;
+	(void)data;
+}
